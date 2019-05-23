f8e54d8a1f57a8e5d5a1eb9c9a2a287ff0bf5976
nickyzj <reg.zj@outlook.com>
2019-05-23 11:57:15 +0800

    add test scripts

--- test_script/smoke_tests.py
+++ test_script/smoke_tests.py
@@ -5,4 +5,4 @@ from xmlrunner import xmlrunner
 chain_monitor_test = unittest.TestLoader().loadTestsFromTestCase(ChainMonitorTest)
 smoke_tests = unittest.TestSuite([chain_monitor_test])
 
-xmlrunner.XMLTestRunner(verbosity=2, output='test_report').run(smoke_tests)
+xmlrunner.XMLTestRunner(verbosity=2, output=r'test_script\test_report').run(smoke_tests)
